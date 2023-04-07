736d5e7f5eae9d41c7c202f87261758503994b28
Jen Tak <goodhoko@gmail.com>
2023-04-07 21:41:14 +0200

    Fetch html pages recursively

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,78 @@
-use eyre::Result;
-use std::{fs, fs::File, io::Write, path::Path};
+use eyre::{eyre, Result};
+use reqwest::{blocking::Response, header::CONTENT_TYPE};
+use std::{
+    collections::HashSet,
+    fs,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+    str,
+};
 use url::Url;
 
 const OUTPUT_DIR: &str = "./out";
 
+struct State {
+    urls_in_process: HashSet<Url>,
+    origin: Url,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    process_url(Url::parse("https://zadnyspe.ch/")?)?;
+    let url = Url::parse("http://localhost:4000/")?;
+    process_url(
+        url.clone(),
+        &mut State {
+            urls_in_process: HashSet::new(),
+            origin: url,
+        },
+    )?;
+
     Ok(())
 }
 
-fn process_url(url: Url) -> Result<()> {
-    println!("Processing {}", url);
-    let response = reqwest::blocking::get(url)?;
-    let url = response.url().clone();
+fn process_url(url: Url, state: &mut State) -> Result<()> {
+    match should_descent(&url, state) {
+        ShouldDescend::No(reason) => {
+            println!("skipping {url}: {reason}");
+            return Ok(());
+        }
+        ShouldDescend::Yes => {}
+    }
 
+    println!("processing {url}");
+
+    let response = reqwest::blocking::get(url.clone())?;
+    let content_type = response
+        .headers()
+        .get(CONTENT_TYPE)
+        .ok_or(eyre!("cannot get content type"))?
+        .to_str()?;
+
+    // Get the final URL after redirects
+    let final_url = response.url().clone();
+
+    // Mark both urls as being processed before descending down
+    state.urls_in_process.insert(url);
+    state.urls_in_process.insert(final_url.clone());
+
+    let content = match content_type {
+        _ if content_type.starts_with("text/html") => process_html(response, state),
+        _ => process_file(response, state),
+    }?;
+
+    // Persist the file to disk
+    let path = url_to_path(&final_url);
+    fs::create_dir_all(path.parent().expect("Parent directory exists"))?;
+    let mut file = File::create(path)?;
+    file.write_all(&content)?;
+
+    Ok(())
+}
+
+fn process_html(response: Response, state: &mut State) -> Result<Vec<u8>> {
+    let url = response.url().clone();
     let content = response.text()?;
     let dom = tl::parse(&content, tl::ParserOptions::default())?;
     for node in dom.nodes().iter() {
@@ -27,19 +84,61 @@ fn process_url(url: Url) -> Result<()> {
                     Some(Some(href)) => href,
                 };
 
-                println!("about to process {:?}", href);
+                let Ok(href) = str::from_utf8(href.as_bytes()) else {
+                    println!("skipping {:?}: can't decode it as utf8", href);
+                    continue;
+                };
+
+                let Ok(resolved_href) = url.join(href) else {
+                    println!("skipping {}: can't join it onto {}", href, url);
+                    continue;
+                };
+
+                if let Err(err) = process_url(resolved_href.clone(), state) {
+                    println!("skipping {}: failed to process: {}", resolved_href, err);
+                    continue;
+                }
             }
             _ => {}
         };
     }
 
-    let dest = Path::new(OUTPUT_DIR).join(&url.path()[1..]);
-    if !dest.exists() {
-        fs::create_dir_all(&dest)?
+    Ok(content.clone().into_bytes())
+}
+
+fn process_file(_response: Response, _state: &mut State) -> Result<Vec<u8>> {
+    Err(eyre!("Not implemented"))
+}
+
+fn url_to_path(url: &Url) -> PathBuf {
+    // TODO: distinguish HTML files that go into ".../name/index.html" and all other files that go into ".../name"
+    Path::new(OUTPUT_DIR)
+        .join(&url.path()[1..])
+        .join("index.html")
+}
+
+enum ShouldDescend {
+    No(&'static str),
+    Yes,
+}
+
+fn should_descent(url: &Url, state: &State) -> ShouldDescend {
+    if state.urls_in_process.contains(url) {
+        return ShouldDescend::No("already being processed");
     }
 
-    let mut file = File::create(dest.join("index.html"))?;
-    file.write_all(content.as_bytes())?;
+    let host = match url.host() {
+        Some(h) => h,
+        None => return ShouldDescend::No("url with no host"),
+    };
+    let current_host = state
+        .origin
+        .host()
+        .expect("existence of host was enforced before");
 
-    Ok(())
+    if host != current_host {
+        return ShouldDescend::No("different host");
+    }
+
+    ShouldDescend::Yes
 }
